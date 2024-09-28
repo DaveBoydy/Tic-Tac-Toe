f4184d93fd4f6ec353a71126da3817a8d27d798f
David Boyd <dboyd2005@msn.com>
2024-09-28 11:35:37 +0100

    Refactor code to add the pubsub design pattern

--- script.js
+++ script.js
@@ -74,6 +74,10 @@
   function displayController() {
     const board = gameBoard();
     board.printBoard();
+
+    pubsub.subscribe("boardClicked", function handleBoardClick(data) {
+      console.log(`The display controller noticed ${data} was clicked`);
+    });
   }
 
   function cacheDom() {
@@ -90,39 +94,46 @@
     const boardView = cacheDom();
 
     boardView.getBoardView().addEventListener("click", (e) => {
-      const target = e.target;
-
-      switch (target.id) {
-        case "cell-one":
-          console.log("cell one was clicked");
-          break;
-        case "cell-two":
-          console.log("cell two was clicked");
-          break;
-        case "cell-three":
-          console.log("cell three was clicked");
-          break;
-
-        case "cell-four":
-          console.log("cell four was clicked");
-          break;
-        case "cell-five":
-          console.log("cell five was clicked");
-          break;
-        case "cell-six":
-          console.log("cell six was clicked");
-          break;
-
-        case "cell-seven":
-          console.log("cell seven was clicked");
-          break;
-        case "cell-eight":
-          console.log("cell eight was clicked");
-          break;
-        case "cell-nine":
-          console.log("cell nine was clicked");
-          break;
-      }
+      const target = e.target.id;
+      pubsub.publish("boardClicked", target);
     });
   }
+
+  /*
+   ** - subscribe(eventName, func) -> subscribe a function to trigger
+   ** when a specific event is published.
+   *
+   ** - unsubscribe(eventName, func) -> remove function from being
+   ** triggered when the event is published.
+   *
+   ** - publish(eventName, func) -> publishes an event, and calls
+   ** all functions associated with the event.
+   */
+  const pubsub = {
+    events: {},
+
+    subscribe(eName, fn) {
+      console.log(`PUBSUB: someone just subscribed to know about ${eName}`);
+      //Check if an event exists, if it doesn't add the event as an empty array.
+      this.events[eName] = this.events[eName] || [];
+      //add the function that gets passed as an argument to the array.
+      this.events[eName].push(fn);
+    },
+    unsubscribe(eName, fn) {
+      console.log(`PUBSUB: someone just Unsubscribed from ${eName}`);
+      //remove an event function by name
+      if (this.events[eName]) {
+        this.events[eName] = this.events[eName].filter((f) => f !== fn);
+      }
+    },
+    publish(eName, data) {
+      console.log(`PUBSUB: Making a broadcast about ${eName} with ${data}`);
+      //emit|publish|announce the event to anyone who is subscribed
+      if (this.events[eName]) {
+        this.events[eName].forEach((f) => {
+          f(data);
+        });
+      }
+    },
+  };
 })();
